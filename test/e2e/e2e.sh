7cad33be8047ef49c75a187d98a5b534b981fb4b
IlanRosenbaum <Ilan@al.net>
2022-06-15 16:52:04 -0700

    Seperate tests into catagorys instead of all being in one file
    Don't allow e2e.sh to used cached tests
    
    Change what digest_auth module we use

--- test/e2e/e2e.sh
+++ test/e2e/e2e.sh
@@ -7,7 +7,7 @@ echo "=====Loading marklogc images to minikube cluster"
 minikube image load marklogic-centos/marklogic-server-centos:10-internal
 
 echo "=====Running tests"
-go test -v ./test/e2e/...
+go test -v -count=1 ./test/e2e/...
 
 echo "=====Delete minikube cluster"
 minikube delete
---
+++ test/e2e/install_test.go
@@ -0,0 +1,66 @@
+package e2e
+
+import (
+	"crypto/tls"
+	"fmt"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/gruntwork-io/terratest/modules/helm"
+	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
+	"github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/gruntwork-io/terratest/modules/random"
+)
+
+func TestHelmInstall(t *testing.T) {
+	// Path to the helm chart we will test 
+	helmChartPath, e := filepath.Abs("../../charts")
+	if (e != nil) {
+		t.Fatalf(e.Error())
+	}
+	namespaceName := "marklogic-" + strings.ToLower(random.UniqueId())
+	kubectlOptions := k8s.NewKubectlOptions("", "", namespaceName)
+	options := &helm.Options{
+		KubectlOptions: kubectlOptions,
+		SetValues: map[string]string{
+			"persistence.enabled": "false",
+			"replicaCount":        "1",
+			"image.repository":    "marklogic-centos/marklogic-server-centos",
+			"image.tag":           "10-internal",
+		},
+	}
+
+	t.Logf("====Creating namespace: " + namespaceName)
+	k8s.CreateNamespace(t, kubectlOptions, namespaceName)
+	
+	defer t.Logf("====Deleting namespace: " + namespaceName)
+	defer k8s.DeleteNamespace(t, kubectlOptions, namespaceName)
+
+	t.Logf("====Installing Helm Chart")
+	releaseName := "test-install"
+	helm.Install(t, options, helmChartPath, releaseName)
+
+	tlsConfig := tls.Config{}
+	podName := releaseName + "-marklogic-0"
+	// wait until the pod is in Ready status
+	k8s.WaitUntilPodAvailable(t, kubectlOptions, podName, 10, 15*time.Second)
+	tunnel := k8s.NewTunnel(
+		kubectlOptions, k8s.ResourceTypePod, podName, 7997, 7997)
+	defer tunnel.Close()
+	tunnel.ForwardPort(t)
+	endpoint := fmt.Sprintf("http://%s", tunnel.Endpoint())
+	t.Logf(`Endpoint: %s`, endpoint)
+
+	http_helper.HttpGetWithRetryWithCustomValidation(
+		t,
+		endpoint,
+		&tlsConfig,
+		10,
+		15*time.Second,
+		func(statusCode int, body string) bool {
+			return statusCode == 200
+		},
+	)
+}
