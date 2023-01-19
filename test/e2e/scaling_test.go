b711f8e95df45c2c91051ac150be3a4ced40b705
Barkha Choithani <barkha.choithani@marklogic.com>
2023-01-19 11:41:37 -0800

    added tests for adding/removing ML hosts

---
+++ test/e2e/scaling_test.go
@@ -0,0 +1,207 @@
+package e2e
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/gruntwork-io/terratest/modules/helm"
+	"github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/tidwall/gjson"
+	digestAuth "github.com/xinsnake/go-http-digest-auth-client"
+)
+
+func TestHelmScaleUp(t *testing.T) {
+	// Path to the helm chart we will test
+	helmChartPath, e := filepath.Abs("../../charts")
+	if e != nil {
+		t.Fatalf(e.Error())
+	}
+	imageRepo, repoPres := os.LookupEnv("dockerRepository")
+	imageTag, tagPres := os.LookupEnv("dockerVersion")
+	username := "admin"
+	password := "admin"
+
+	if !repoPres {
+		imageRepo = "marklogicdb/marklogic-db"
+		t.Logf("No imageRepo variable present, setting to default value: " + imageRepo)
+	}
+
+	if !tagPres {
+		imageTag = "latest"
+		t.Logf("No imageTag variable present, setting to default value: " + imageTag)
+	}
+
+	namespaceName := "marklogic-" + strings.ToLower(random.UniqueId())
+	kubectlOptions := k8s.NewKubectlOptions("", "", namespaceName)
+	options := &helm.Options{
+		KubectlOptions: kubectlOptions,
+		SetValues: map[string]string{
+			"persistence.enabled":   "false",
+			"replicaCount":          "1",
+			"image.repository":      imageRepo,
+			"image.tag":             imageTag,
+			"auth.adminUsername":    username,
+			"auth.adminPassword":    password,
+			"logCollection.enabled": "false",
+		},
+	}
+
+	t.Logf("====Creating namespace: " + namespaceName)
+	k8s.CreateNamespace(t, kubectlOptions, namespaceName)
+	defer t.Logf("====Deleting namespace: " + namespaceName)
+	defer k8s.DeleteNamespace(t, kubectlOptions, namespaceName)
+
+	t.Logf("====Installing Helm Chart")
+	releaseName := "test-upgrade"
+	helm.Install(t, options, helmChartPath, releaseName)
+
+	newOptions := &helm.Options{
+		KubectlOptions: kubectlOptions,
+		SetValues: map[string]string{
+			"persistence.enabled":   "false",
+			"replicaCount":          "2",
+			"image.repository":      imageRepo,
+			"image.tag":             imageTag,
+			"logCollection.enabled": "false",
+		},
+	}
+
+	t.Logf("====Upgrading Helm Chart")
+	helm.Upgrade(t, newOptions, helmChartPath, releaseName)
+
+	podName := releaseName + "-marklogic-1"
+
+	// wait until second pod is in Ready status
+	k8s.WaitUntilPodAvailable(t, kubectlOptions, podName, 10, 20*time.Second)
+
+	tunnel := k8s.NewTunnel(
+		kubectlOptions, k8s.ResourceTypePod, podName, 8002, 8002)
+	defer tunnel.Close()
+	tunnel.ForwardPort(t)
+
+	hostsEndpoint := fmt.Sprintf("http://%s/manage/v2/hosts?view=status&format=json", tunnel.Endpoint())
+	t.Logf(`Endpoint: %s`, hostsEndpoint)
+
+
+	getHostsDR := digestAuth.NewRequest(username, password, "GET", hostsEndpoint, "")
+
+	resp, err := getHostsDR.Execute()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	totalHosts := gjson.Get(string(body), `host-status-list.status-list-summary.total-hosts.value`)
+	t.Logf(`totalHosts: = %s`, totalHosts)
+
+	// verify total number of hosts on the clsuter after scaling up
+	if totalHosts.Num != 2 {
+		t.Errorf("Incorrect number of offline hosts")
+	}
+}
+
+func TestHelmScaleDown(t *testing.T) {
+	// Path to the helm chart we will test
+	helmChartPath, e := filepath.Abs("../../charts")
+	if e != nil {
+		t.Fatalf(e.Error())
+	}
+	imageRepo, repoPres := os.LookupEnv("dockerRepository")
+	imageTag, tagPres := os.LookupEnv("dockerVersion")
+	username := "admin"
+	password := "admin"
+
+	if !repoPres {
+		imageRepo = "marklogicdb/marklogic-db"
+		t.Logf("No imageRepo variable present, setting to default value: " + imageRepo)
+	}
+
+	if !tagPres {
+		imageTag = "latest"
+		t.Logf("No imageTag variable present, setting to default value: " + imageTag)
+	}
+
+	namespaceName := "marklogic-" + strings.ToLower(random.UniqueId())
+	kubectlOptions := k8s.NewKubectlOptions("", "", namespaceName)
+	options := &helm.Options{
+		KubectlOptions: kubectlOptions,
+		SetValues: map[string]string{
+			"persistence.enabled":   "false",
+			"replicaCount":          "2",
+			"image.repository":      imageRepo,
+			"image.tag":             imageTag,
+			"auth.adminUsername":    username,
+			"auth.adminPassword":    password,
+			"logCollection.enabled": "false",
+		},
+	}
+
+	t.Logf("====Creating namespace: " + namespaceName)
+	k8s.CreateNamespace(t, kubectlOptions, namespaceName)
+	defer t.Logf("====Deleting namespace: " + namespaceName)
+	defer k8s.DeleteNamespace(t, kubectlOptions, namespaceName)
+
+	t.Logf("====Installing Helm Chart")
+	releaseName := "test-upgrade"
+	helm.Install(t, options, helmChartPath, releaseName)
+
+	podName := releaseName + "-marklogic-0"
+
+	// wait until the pod is in Ready status
+	k8s.WaitUntilPodAvailable(t, kubectlOptions, podName, 10, 20*time.Second)
+
+	newOptions := &helm.Options{
+		KubectlOptions: kubectlOptions,
+		SetValues: map[string]string{
+			"persistence.enabled":   "false",
+			"replicaCount":          "1",
+			"image.repository":      imageRepo,
+			"image.tag":             imageTag,
+			"logCollection.enabled": "false",
+		},
+	}
+
+	t.Logf("====Upgrading Helm Chart")
+	helm.Upgrade(t, newOptions, helmChartPath, releaseName)
+
+	time.Sleep(20 * time.Second)
+
+	tunnel := k8s.NewTunnel(
+		kubectlOptions, k8s.ResourceTypePod, podName, 8002, 8002)
+	defer tunnel.Close()
+	tunnel.ForwardPort(t)
+
+	hostsEndpoint := fmt.Sprintf("http://%s/manage/v2/hosts?view=status&format=json", tunnel.Endpoint())
+	t.Logf(`Endpoint: %s`, hostsEndpoint)
+
+
+	getHostsDR := digestAuth.NewRequest(username, password, "GET", hostsEndpoint, "")
+
+	resp, err := getHostsDR.Execute()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	t.Logf(`Body: %s`, string(body))
+
+	totalHostsOffline := gjson.Get(string(body), `host-status-list.status-list-summary.total-hosts-offline.value`)
+	
+	//verify there is a offline host after scaling down
+	if totalHostsOffline.Num != 1 {
+		t.Errorf("Incorrect number of offline hosts")
+	}
+}
