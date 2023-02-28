70df390885dc118c8d2a048199385d4f668d4614
Barkha Choithani <barkha.choithani@marklogic.com>
2023-02-28 09:25:51 -0800

    merged tests for upgrade and used a specific version for upgrade

--- test/e2e/upgrade_test.go
+++ test/e2e/upgrade_test.go
@@ -8,12 +8,15 @@ import (
 	"strings"
 	"testing"
 	"time"
+	"io/ioutil"
 
 	"github.com/gruntwork-io/terratest/modules/helm"
 	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/stretchr/testify/assert"
+	"github.com/tidwall/gjson"
+	digestAuth "github.com/xinsnake/go-http-digest-auth-client"
 )
 
 func TestHelmUpgrade(t *testing.T) {
@@ -105,4 +108,86 @@ func TestHelmUpgrade(t *testing.T) {
 	passwordArr = secret.Data["password"]
 	passwordAfterUpgrade := string(passwordArr[:])
 	assert.Equal(t, passwordAfterUpgrade, passwordAfterInstall)
+}
+
+func TestMLupgrade(t *testing.T) {
+	// Path to the helm chart we will test
+	helmChartPath, e := filepath.Abs("../../charts")
+	if e != nil {
+		t.Fatalf(e.Error())
+	}
+	username := "admin"
+	password := "admin"
+
+	namespaceName := "marklogic-" + strings.ToLower(random.UniqueId())
+	kubectlOptions := k8s.NewKubectlOptions("", "", namespaceName)
+	options := &helm.Options{
+		KubectlOptions: kubectlOptions,
+		SetValues: map[string]string{
+			"persistence.enabled": "false",
+			"replicaCount":        "1",
+			"image.repository":    "marklogicdb/marklogic-db",
+			"image.tag":           "latest-10.0",
+			"auth.adminUsername":  username,
+			"auth.adminPassword":  password,
+		},
+	}
+
+	t.Logf("====Creating namespace: " + namespaceName)
+	k8s.CreateNamespace(t, kubectlOptions, namespaceName)
+	defer t.Logf("====Deleting namespace: " + namespaceName)
+	defer k8s.DeleteNamespace(t, kubectlOptions, namespaceName)
+
+	t.Logf("====Installing Helm Chart")
+	releaseName := "test-ml-upgrade"
+	helm.Install(t, options, helmChartPath, releaseName)
+
+	podName := releaseName + "-marklogic-0"
+
+	// wait until second pod is in Ready status
+	k8s.WaitUntilPodAvailable(t, kubectlOptions, podName, 20, 20*time.Second)
+
+	newOptions := &helm.Options{
+		KubectlOptions: kubectlOptions,
+		SetValues: map[string]string{
+			"persistence.enabled":   "false",
+			"image.repository":      "marklogicdb/marklogic-db",
+			"image.tag":             "latest-11.0",
+			"logCollection.enabled": "false",
+		},
+	}
+
+	t.Logf("====Upgrading Helm Chart")
+	helm.Upgrade(t, newOptions, helmChartPath, releaseName)
+
+	// Give time to change status of pod from running to terminate during upgrade
+	time.Sleep(10 * time.Second)
+
+	// wait until second pod is in Ready status
+	k8s.WaitUntilPodAvailable(t, kubectlOptions, podName, 15, 30*time.Second)
+
+	tunnel := k8s.NewTunnel(
+		kubectlOptions, k8s.ResourceTypePod, podName, 8002, 8002)
+	defer tunnel.Close()
+	tunnel.ForwardPort(t)
+
+	clusterEndpoint := fmt.Sprintf("http://%s/manage/v2?format=json", tunnel.Endpoint())
+	t.Logf(`Endpoint: %s`, clusterEndpoint)
+
+	getMLversion := digestAuth.NewRequest(username, password, "GET", clusterEndpoint, "")
+
+	resp, err := getMLversion.Execute()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	mlVersion := gjson.Get(string(body), `local-cluster-default.version`)
+
+	// verify latest MarkLogic version after upgrade
+	assert.Equal(t, mlVersion.Str, "11.0.0")
+
 }
