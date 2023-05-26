f190c51362c24c8180c6c2cc981ed80c079a19d6
Vitaly Korolev <vitaly.korolev@gmail.com>
2023-05-26 10:11:09 -0700

    java fix

--- test/hc_e2e.sh
+++ test/hc_e2e.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 #This helper script will run E2E UI tests for Hub Central (https://github.com/marklogic/marklogic-data-hub/tree/develop/marklogic-data-hub-central/ui/e2e)
 
+#override java version for Jenkins
+[[ $USER = 'builder' ]] && export PATH=/home/builder/java/jdk1.8.0_201/bin:$PATH
+
 echo "---- start port forwarding ----"
 kubectl port-forward hc-marklogic-0 8000 8001 8002 8010 8011 8013 &
 forwarderPID=$!
 
 echo "---- configure environment ----"
 cd marklogic-data-hub/
-./gradlew -Dhttps.protocols=TLSv1.2 clean build -x test
-./gradlew -Dhttps.protocols=TLSv1.2 publishToMavenLocal -PskipWeb=true
+./gradlew clean build -x test
+./gradlew publishToMavenLocal -PskipWeb=true
 cd marklogic-data-hub-central/ui/e2e
 ./setup.sh dhs=false mlHost=localhost mlSecurityUsername=admin mlSecurityPassword=admin
 
 echo "---- start Hub Central application ----"
 cd ../../..
-./gradlew -Dhttps.protocols=TLSv1.2 bootRun -PhubUseLocalDefaults=true &
+./gradlew bootRun -PhubUseLocalDefaults=true &
 bootRunPID=$!
 
 sleep 10
