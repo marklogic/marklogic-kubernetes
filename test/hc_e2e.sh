d4050c198a1f5e7f867d8739c887cb5efbcf5557
Vitaly Korolev <vitaly.korolev@gmail.com>
2023-06-12 22:26:01 -0700

    switch node version

--- test/hc_e2e.sh
+++ test/hc_e2e.sh
@@ -2,7 +2,7 @@
 #This helper script will run E2E UI tests for Hub Central (https://github.com/marklogic/marklogic-data-hub/tree/develop/marklogic-data-hub-central/ui/e2e)
 
 #override java and node version for Jenkins
-[[ $USER = 'builder' ]] && { export PATH=/home/builder/java/jdk1.8.0_201/bin:/home/builder/nodeJs/node-v16.19.1-linux-x64/bin/:$PATH; unset JAVA_HOME; }
+[[ $USER = 'builder' ]] && { export PATH=/home/builder/java/jdk1.8.0_201/bin:/home/builder/nodeJs/node-v14.15.4-linux-x64/bin/:$PATH; unset JAVA_HOME; }
 
 echo "---- start port forwarding ----"
 kubectl port-forward hc-marklogic-0 8000 8001 8002 8010 8011 8013 &> /dev/null &
